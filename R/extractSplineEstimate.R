883aeb133a7028dc1eeea9850ad00f88633a7958
Yucatan De Groote <yucatan.degroote@dkv.be>
2025-01-09 14:51:40 +0100

    documentation of extractSplineEstimate

--- R/extractSplineEstimate.R
+++ R/extractSplineEstimate.R
@@ -1,22 +1,39 @@
-extractSplineEstimate <- function(gamFit, keyWordColSelection = NULL){
+#' Extract the Spline estimates of the GAM model 
+#'
+#' @param gamFit object gam, with the already runned GAM model of [mgcv::gam()]
+#' @param keyWordColSelection list with the smooth terms to be extracted from the GAM. 
+#' If NULL all the smooth terms will be used
+#'
+#' @returns list of length of the `keyWordColSelection` containing a list with all the estimates in the GAM. 
+#' The number of lines will then be the number of rows of the given dataset. 
+#' @export
+extractSplineEstimate <- function(gamFit, 
+                                  keyWordColSelection = NULL){
   
   allSmoothTerms <- laply(gamFit$smooth, function(xx) xx$vn[1])
-  allSmoothTerms[which(!allSmoothTerms %in% c('longitude', 'latitude'))]
+
   if(is.null(keyWordColSelection)){
     keyWordColSelection <- allSmoothTerms
   } else {
     present <- (keyWordColSelection %in% allSmoothTerms)
-    if(length(present) != length(keyWordColSelection)){
-      sprintf("The elements %s of the 'keyWordColSelection' argument are not part of the smooth terms of the 'gamFit' argument.", paste(keyWordColSelection[!present], collapse = ', '))
+    if(!all(present)){
+      sprintf("The elements %s of the 'keyWordColSelection' argument are not part of the smooth terms of the 'gamFit' argument.", 
+              paste(keyWordColSelection[!present], collapse = ', '))
     }
   }
-  if(sum(class(gamFit) %in% 'gam') == 0) stop('The "gamFit" argument should be an output object of the mgcv::gam function.')
+  
+  if (!any(class(gamFit) %in% 'gam')) {
+    stop('The "gamFit" argument should be an output object of the mgcv::gam function.')
+  } 
   gamFitModelMatrix <- model.matrix(gamFit)
+  
   fittedSpline <- list()
   length(fittedSpline) <- length(keyWordColSelection)
-  for(iSpline in 1:length(fittedSpline)){
+  
+  for (iSpline in 1:length(fittedSpline)) {
     selectionCols <- grep(keyWordColSelection[iSpline], names(gamFit$coefficients))
-    fittedSpline[[iSpline]] <- gamFitModelMatrix[, selectionCols]%*%gamFit$coefficients[selectionCols]
+    fittedSpline[[iSpline]] <- gamFitModelMatrix[, selectionCols] %*% 
+      gamFit$coefficients[selectionCols]
   }
   names(fittedSpline) <- keyWordColSelection
   return(fittedSpline)
