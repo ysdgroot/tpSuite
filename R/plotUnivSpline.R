c8edbc97a9836b8263b0de78ddb8a2ab5f453e8f
Yucatan De Groote <yucatan.degroote@dkv.be>
2025-01-10 16:45:30 +0100

    Adding documentation for the function plotUnivSpline

--- R/plotUnivSpline.R
+++ R/plotUnivSpline.R
@@ -1,25 +1,24 @@
-#' Title
+#' Plot the univariate spline of a GAM model
 #'
-#' @param gamFit 
-#' @param inputDT 
-#' @param selectedVar 
-#' @param simultaneousCI 
-#' @param transparence 
-#' @param colorLine 
-#' @param colorRibbon 
-#' @param lineWidth 
-#' @param showPlot 
-#' @param nSamples 
-#' @param plotDir 
-#' @param plotName 
-#' @param pdf2 
-#' @param xLimits 
-#' @param yLimits 
+#' @param gamFit fitted GAM model, from the package [mgcv]
+#' @param inputDT data.table object which is used to train the GAM model 
+#' @param selectedVar single character variable
+#' @param simultaneousCI logical, if the CI should be simulated
+#' @param transparence alpha value for the ribbon, see [ggplot::geom_ribbon]
+#' @param colorLine colorname or hexcode for the splice
+#' @param colorRibbon colorname or hexcode for the ribbon
+#' @param lineWidth linewidth for the spline
+#' @param showPlot logical, if the plot should be shown or not
+#' @param nSamples numeric, number of samples to be used 
+#' @param plotDir path location for the directory
+#' @param plotName name of the plot, will be saved at location `plotDir`
+#' @param pdf2 logical, if the plot should be saved as a pdf
+#' @param xLimits,yLimits numeric of length 2, indicating the limits of the plots
 #'
-#' @returns
+#' @importFrom mgcv rmvn
+#'
+#' @returns ggplot object of the spline
 #' @export
-#'
-#' @examples
 plotUnivSpline <- function(gamFit, 
                            inputDT, 
                            selectedVar, 
@@ -46,9 +45,13 @@ plotUnivSpline <- function(gamFit,
   if (!is.data.table(inputDT)) {
     stop('The "inputDT" argument should be a data.table object.')
   }
-  if (length(selectedVar) < 1 & selectedVar %in% names(inputDT) != length(selectedVar)) {
+  if (length(selectedVar) < 1) {
     stop('The "selectedVar" argument should be a character vector consisting of column names of the "inputDT" argument.')
   }
+  if (length(selectedVar) > 1){
+    stop('The "selectedVar" argument should only 1 variable to plot')
+  }
+  
   
   if (!is.null(xLimits)) {
     if (length(xLimits) != 2 | !is.numeric(xLimits)) {
@@ -61,22 +64,12 @@ plotUnivSpline <- function(gamFit,
     } 
   }
   
-  for (var in selectedVar) {
-    varNumber <- 1
-    goOn <- TRUE
-    
-    while (goOn) {
-      modelVar <- try(attr(gamFit$terms[varNumber], 'term.labels'), 
-                      silent = TRUE)
-      if (class(modelVar) == "try-error") {
-        stop(paste(var, 'is not part of the model variables of the "gamFit" argument.'))
-      } 
-      if (var == modelVar) {
-        goOn <- FALSE
-      } 
-      varNumber <- varNumber + 1
-    }
+  if (!(selectedVar %in% attr(gamFit$terms, 'term.labels'))) {
+    stop(sprintf("%s is not a model variable of 'gamFit' with smoothed results", 
+                 selectedVar))
   }
+  
+  
   
   # end checks
   
@@ -89,107 +82,117 @@ plotUnivSpline <- function(gamFit,
   refDF <- data.frame(t(c(1, rep(0, length(numericVarNames)), referenceClass)))
   names(refDF) <- c('exposure', numericVarNames, categoricalVarNames)
   
-  ggplotObject <- list()
   coVarMatrix <- vcov(gamFit)
-  N <- 10000
   
-  for (iVar in 1:length(selectedVar)) {
+  newDataVar <- seq(min(inputDT[[which(names(inputDT) == selectedVar)]], 
+                        na.rm = TRUE), 
+                    max(inputDT[[which(names(inputDT) == selectedVar)]], 
+                        na.rm = TRUE), 
+                    length = nSamples)
+  
+  newData <- data.frame(refDF, 
+                        temp = rep(-1, length(newDataVar)))
+  newData <- newData[,-ncol(newData)]
+  newData[, which(names(refDF) == selectedVar)] <- newDataVar
+  newData <- as.data.table(newData)
+  
+  categoricalCols <- which(names(newData) %in% names(gamFit$xlevels))
+  
+  numericCols <- 1:ncol(newData)
+  numericCols <- numericCols[-categoricalCols]
+  names(newData)[numericCols]
+  
+  for(iCol in numericCols){
+    newData[[iCol]] <- as.numeric(newData[[iCol]])
+  }
+  
+  if(simultaneousCI == TRUE){
     
-    newDataVar <- seq(min(inputDT[[which(names(inputDT) == selectedVar)]], 
-                          na.rm = TRUE), 
-                      max(inputDT[[which(names(inputDT) == selectedVar)]], 
-                          na.rm = TRUE), 
-                      length = nSamples)
+    predictions <- predict(gamFit, 
+                           newData, 
+                           se.fit = TRUE)
+    fitSE <- predictions$se.fit
     
-    newData <- data.frame(refDF, 
-                          temp = rep(-1, length(newDataVar)))
-    newData <- newData[,-ncol(newData)]
-    newData[, which(names(refDF) == selectedVar)] <- newDataVar
-    newData <- as.data.table(newData)
+    N <- 10000
+    normalSamples <- mgcv::rmvn(N,
+                                mu = rep(0, nrow(coVarMatrix)), 
+                                V = coVarMatrix)
+    Cg <- predict(gamFit, 
+                  newData, 
+                  type = "lpmatrix")
+    simulatedDiff <- Cg %*% t(normalSamples)
+    absoluteDiff <- abs(sweep(simulatedDiff, 1, fitSE, FUN = "/"))
+    maxDiff <- apply(absoluteDiff, 2L, max)
+    criticalValue <- quantile(maxDiff, 
+                              prob = 0.95, 
+                              type = 8)
     
-    categoricalCols <- which(names(newData) %in% names(gamFit$xlevels))
+    upperBound <- splineEst + criticalValue*stError
+    lowerBound <- splineEst - criticalValue*stError
     
-    numericCols <- 1:ncol(newData)
-    numericCols <- numericCols[-categoricalCols]
-    names(newData)[numericCols]
+  } else {
     
-    for(iCol in numericCols){
-      newData[[iCol]] <- as.numeric(newData[[iCol]])
-    }
+    predictions <- predict(gamFit, 
+                           newData, 
+                           type = "terms", 
+                           se.fit = TRUE)
+    fitSE <- predictions$se.fit
     
-    if(simultaneousCI == TRUE){
-      
-      predictions <- predict(gamFit, newData, se.fit = TRUE)
-      fitSE <- predictions$se.fit
-      
-      normalSamples <- rMVN(N, mu = rep(0, nrow(coVarMatrix)), sig = coVarMatrix)
-      Cg <- predict(gamFit, newData, type = "lpmatrix")
-      simulatedDiff <- Cg %*% t(normalSamples)
-      absoluteDiff <- abs(sweep(simulatedDiff, 1, fitSE, FUN = "/"))
-      maxDiff <- apply(absoluteDiff, 2L, max)
-      criticalValue <- quantile(maxDiff, prob = 0.95, type = 8)
-      
-      upperBound <- splineEst + criticalValue*stError
-      lowerBound <- splineEst - criticalValue*stError
-      
-    } else {
-      
-      predictions <- predict(gamFit, newData, type = "terms", se.fit = TRUE)
-      fitSE <- predictions$se.fit
-      
-      selectedVarSpline <- paste(paste('s(', selectedVar, sep = ''), ')', sep = '')
-      selectedCol <- grep(selectedVarSpline, colnames(fitSE), fixed = TRUE)
-      stError <- fitSE[, selectedCol]
-      
-      Xp <- predict(gamFit, newdata = newData, type = "lpmatrix")
-      
-      selectedCol <- grep(selectedVarSpline, colnames(Xp), fixed = TRUE)
-      splineEst <- Xp[, selectedCol]%*%gamFit$coefficients[selectedCol]
-      
-      upperBound <- splineEst + 2*stError
-      lowerBound <- splineEst - 2*stError
-      
-    }
-    ggplotData <- as.data.table(data.frame(xVar = newDataVar, 
-                                           yVar = splineEst, 
-                                           upperBound = upperBound, 
-                                           lowerBound = lowerBound))
+    selectedVarSpline <- paste(paste('s(', selectedVar, sep = ''), ')', sep = '')
+    selectedCol <- grep(selectedVarSpline, 
+                        colnames(fitSE), 
+                        fixed = TRUE)
+    stError <- fitSE[, selectedCol]
     
-    ggplot_p <- ggplot(ggplotData, 
-                       aes(x = xVar, 
-                           y = yVar)) + 
-      geom_line(lwd = lineWidth, 
-                color = colorLine) +
-      geom_ribbon(aes(ymin = lowerBound, 
-                      ymax = upperBound), 
-                  alpha = transparence, 
-                  fill = colorRibbon) +
-      xlab(selectedVar[iVar]) + 
-      ylab('Predictions + uncertainty')
+    Xp <- predict(gamFit, 
+                  newdata = newData, 
+                  type = "lpmatrix")
     
-    if (!is.null(xLimits)) {
-      ggplot_p <- ggplot_p +
-        coord_cartesian(xlim = xLimits)
-    }
+    selectedCol <- grep(selectedVarSpline, 
+                        colnames(Xp), 
+                        fixed = TRUE)
+    splineEst <- Xp[, selectedCol] %*% gamFit$coefficients[selectedCol]
     
-    if (!is.null(yLimits)) {
-      ggplot_p <- ggplot_p + 
-        coord_cartesian(ylim = yLimits)
-    }
-    
-    ggplotObject[[iVar]] <- ggplot_p
-    
-    if(showPlot == TRUE) {
-      print(ggplot_p)
-    }
-    
-    #TODO: this should be put into a function that takes a ggplot object to plot 
-    if (!is.null(plotDir) & !is.null(plotName)){
-      if (pdf2) ggsave(paste(plotDir, paste(plotName, '.pdf', sep = ''), sep = ""))
-      ggsave(paste(plotDir, paste(plotName, '.png', sep = ''), sep = ""))
-    }
+    upperBound <- splineEst + 2*stError
+    lowerBound <- splineEst - 2*stError
     
   }
-  return(ggplotObject)
+  ggplotData <- as.data.table(data.frame(xVar = newDataVar, 
+                                         yVar = splineEst, 
+                                         upperBound = upperBound, 
+                                         lowerBound = lowerBound))
+  
+  ggplot_p <- ggplot(ggplotData, 
+                     aes(x = xVar, 
+                         y = yVar)) + 
+    geom_line(lwd = lineWidth, 
+              color = colorLine) +
+    geom_ribbon(aes(ymin = lowerBound, 
+                    ymax = upperBound), 
+                alpha = transparence, 
+                fill = colorRibbon) +
+    xlab(selectedVar) + 
+    ylab('Predictions + uncertainty')
+  
+  if (!is.null(xLimits)) {
+    ggplot_p <- ggplot_p +
+      coord_cartesian(xlim = xLimits)
+  }
+  
+  if (!is.null(yLimits)) {
+    ggplot_p <- ggplot_p + 
+      coord_cartesian(ylim = yLimits)
+  }
+  
+  if (showPlot == TRUE) {
+    print(ggplot_p)
+  }
+  
+  if (!is.null(plotDir) & !is.null(plotName)){
+    if (pdf2) ggsave(paste(plotDir, paste(plotName, '.pdf', sep = ''), sep = ""))
+    ggsave(paste(plotDir, paste(plotName, '.png', sep = ''), sep = ""))
+  }
+    
+  return(ggplot_p)
 }
 
